8549346e163aa383874b26ac08bc80b4a4f779ce
Avdo Sator <avdo.sator@hotmail.com>
2024-10-18 17:16:37 +0200

    Create service structure for Genre

---
+++ src/main/java/com/cinemaapp/backend/service/impl/GenreServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cinemaapp.backend.service.impl;
+
+import com.cinemaapp.backend.service.GenreService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreServiceImpl implements GenreService {
+}
