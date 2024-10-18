1be3b820e4d19833f336a59e747dcd242ae05a82
Avdo Sator <avdo.sator@hotmail.com>
2024-10-18 18:01:29 +0200

    Create controller structure for Venue

---
+++ src/main/java/com/cinemaapp/backend/controller/dto/CreateVenueDto.java
@@ -0,0 +1,6 @@
+package com.cinemaapp.backend.controller.dto;
+
+import com.cinemaapp.backend.service.domain.request.CreateVenueRequest;
+
+public class CreateVenueDto extends CreateVenueRequest {
+}
