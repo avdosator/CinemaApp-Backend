c62ee2faddf6cb6bf5f4a311f1e30550cd546e06
Avdo Sator <avdo.sator@hotmail.com>
2024-10-18 17:42:13 +0200

    Create controller structure for Photo

---
+++ src/main/java/com/cinemaapp/backend/controller/dto/CreatePhotoDto.java
@@ -0,0 +1,6 @@
+package com.cinemaapp.backend.controller.dto;
+
+import com.cinemaapp.backend.service.domain.request.CreatePhotoRequest;
+
+public class CreatePhotoDto extends CreatePhotoRequest {
+}
