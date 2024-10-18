75549d67bdf58325df7c07850403c96b7f5fb7bf
Avdo Sator <avdo.sator@hotmail.com>
2024-10-18 17:17:41 +0200

    Create GenreController and DTO

---
+++ src/main/java/com/cinemaapp/backend/controller/dto/CreateGenreDto.java
@@ -0,0 +1,6 @@
+package com.cinemaapp.backend.controller.dto;
+
+import com.cinemaapp.backend.service.domain.request.CreateGenreRequest;
+
+public class CreateGenreDto extends CreateGenreRequest {
+}
