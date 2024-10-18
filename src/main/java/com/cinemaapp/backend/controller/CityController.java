024833c74ddfe3c07786dcf7b6dcfcb140255f04
Avdo Sator <avdo.sator@hotmail.com>
2024-10-18 14:40:31 +0200

    Create controller class and DTO class for city

--- src/main/java/com/cinemaapp/backend/controller/CityController.java
+++ src/main/java/com/cinemaapp/backend/controller/CityController.java
@@ -3,5 +3,5 @@ package com.cinemaapp.backend.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class CityController {
 }
---
+++ src/main/java/com/cinemaapp/backend/controller/dto/CreateCityDto.java
@@ -0,0 +1,6 @@
+package com.cinemaapp.backend.controller.dto;
+
+import com.cinemaapp.backend.service.domain.request.CreateCityRequest;
+
+public class CreateCityDto extends CreateCityRequest {
+}
