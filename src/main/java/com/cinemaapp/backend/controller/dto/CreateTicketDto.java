58b3edb482f8b2a84ac7a473c8992655d3055230
Avdo Sator <avdo.sator@hotmail.com>
2024-10-19 12:56:54 +0200

    Create controller structure for Ticket

---
+++ src/main/java/com/cinemaapp/backend/controller/dto/CreateTicketDto.java
@@ -0,0 +1,6 @@
+package com.cinemaapp.backend.controller.dto;
+
+import com.cinemaapp.backend.service.domain.request.CreateTicketRequest;
+
+public class CreateTicketDto extends CreateTicketRequest {
+}
