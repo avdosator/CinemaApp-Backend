68df53d03d078181ce3f11e66e5c8171afecdd39
Avdo Sator <avdo.sator@hotmail.com>
2024-10-28 10:49:02 +0100

    Add code for seeding city table

--- src/main/java/com/cinemaapp/backend/config/DatabaseInitializer.java
+++ src/main/java/com/cinemaapp/backend/config/DatabaseInitializer.java
@@ -1,10 +1,13 @@
 package com.cinemaapp.backend.config;
 
-import com.cinemaapp.backend.repository.*;
+import com.cinemaapp.backend.repository.entity.*;
 import com.cinemaapp.backend.repository.crud.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
@@ -29,6 +32,26 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        // seed city table
+        if (crudCityRepository.findAll().isEmpty()) {
+            CityEntity sarajevo = new CityEntity();
+            sarajevo.setName("Sarajevo");
+            sarajevo.setPostalCode(71000);
+            sarajevo.setCountry("Bosnia and Herzegovina");
+            sarajevo.setCreatedAt(LocalDateTime.now());
+            sarajevo.setUpdatedAt(LocalDateTime.now());
+
+            CityEntity mostar = new CityEntity();
+            mostar.setName("Mostar");
+            mostar.setPostalCode(88000);
+            mostar.setCountry("Bosnia and Herzegovina");
+            mostar.setCreatedAt(LocalDateTime.now());
+            mostar.setUpdatedAt(LocalDateTime.now());
+
+            crudCityRepository.saveAll(Arrays.asList(sarajevo, mostar));
+        }
+
         
     }
 }
