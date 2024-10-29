799babcddc45e089346f45c82399851b078c6b05
Avdo Sator <avdo.sator@hotmail.com>
2024-10-29 21:39:21 +0100

    Create Page class that will be our representation of spring's Page class

---
+++ src/main/java/com/cinemaapp/backend/controller/dto/Page.java
@@ -0,0 +1,52 @@
+package com.cinemaapp.backend.controller.dto;
+
+import java.util.List;
+
+public class Page<T> {
+
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private int totalElements;
+    private int totalPages;
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(int totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}
