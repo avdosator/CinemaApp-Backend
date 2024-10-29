ea5737bccfc163a2cff9ecf4a8073fe7849e6a5f
Avdo Sator <avdo.sator@hotmail.com>
2024-10-29 22:48:21 +0100

    Create MovieSpecification class that will be used for filtering movie results

---
+++ src/main/java/com/cinemaapp/backend/repository/specification/MovieSpecification.java
@@ -0,0 +1,17 @@
+package com.cinemaapp.backend.repository.specification;
+
+import com.cinemaapp.backend.repository.entity.MovieEntity;
+import com.cinemaapp.backend.repository.entity.ProjectionEntity;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+
+public class MovieSpecification {
+
+    public static Specification<MovieEntity> hasActiveProjection() {
+        return (root, query, criteriaBuilder) -> {
+            Join<MovieEntity, ProjectionEntity> projections = root.join("projectionEntities");
+            // Check if any projection has status "active"
+            return criteriaBuilder.equal(projections.get("status"), "active");
+        };
+    }
+}
