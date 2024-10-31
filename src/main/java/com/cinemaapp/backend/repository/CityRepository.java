8b82d46cfba0f803e285e8a4890f80d49dfaf5cb
Avdo Sator <avdo.sator@hotmail.com>
2024-10-31 15:35:23 +0100

    Add methods for finding all cities in databaase

--- src/main/java/com/cinemaapp/backend/repository/CityRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/CityRepository.java
@@ -1,4 +1,10 @@
 package com.cinemaapp.backend.repository;
 
+import com.cinemaapp.backend.service.domain.model.City;
+
+import java.util.List;
+
 public interface CityRepository {
+
+    List<City> findAllCities();
 }
--- src/main/java/com/cinemaapp/backend/repository/impl/CityJpaRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/impl/CityJpaRepository.java
@@ -1,8 +1,32 @@
 package com.cinemaapp.backend.repository.impl;
 
 import com.cinemaapp.backend.repository.CityRepository;
+import com.cinemaapp.backend.repository.crud.CrudCityRepository;
+import com.cinemaapp.backend.repository.entity.CityEntity;
+import com.cinemaapp.backend.service.domain.model.City;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class CityJpaRepository implements CityRepository {
+
+    private final CrudCityRepository crudCityRepository;
+
+    @Autowired
+    public CityJpaRepository(CrudCityRepository crudCityRepository) {
+        this.crudCityRepository = crudCityRepository;
+    }
+
+    @Override
+    public List<City> findAllCities() {
+        List<CityEntity> cityEntities = crudCityRepository.findAll();
+        List<City> cities = new ArrayList<>();
+        for (CityEntity cityEntity : cityEntities) {
+            cities.add(cityEntity.toDomainModel());
+        }
+        return cities;
+    }
 }
