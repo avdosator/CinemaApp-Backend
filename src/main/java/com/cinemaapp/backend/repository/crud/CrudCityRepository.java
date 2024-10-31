85e687d899529feb7de6865e2ffdb33ccbdccf6e
Avdo Sator <avdo.sator@hotmail.com>
2024-10-31 20:51:23 +0100

    Add methods for searching entity by name/title

--- src/main/java/com/cinemaapp/backend/repository/crud/CrudCityRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/crud/CrudCityRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CrudCityRepository extends JpaRepository<CityEntity, UUID> {
+
+    CityEntity findByName(String name);
 }
