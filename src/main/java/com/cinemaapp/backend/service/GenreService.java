8039b576eefd969dd71e3589ace28ac197ee1456
Avdo Sator <avdo.sator@hotmail.com>
2024-10-31 22:56:27 +0100

    Implement service and repository logic for fetching genres

--- src/main/java/com/cinemaapp/backend/service/GenreService.java
+++ src/main/java/com/cinemaapp/backend/service/GenreService.java
@@ -1,4 +1,10 @@
 package com.cinemaapp.backend.service;
 
+import com.cinemaapp.backend.service.domain.model.Genre;
+
+import java.util.List;
+
 public interface GenreService {
+
+    List<Genre> findAllGenres();
 }
--- src/main/java/com/cinemaapp/backend/service/impl/GenreServiceImpl.java
+++ src/main/java/com/cinemaapp/backend/service/impl/GenreServiceImpl.java
@@ -1,8 +1,25 @@
 package com.cinemaapp.backend.service.impl;
 
+import com.cinemaapp.backend.repository.GenreRepository;
 import com.cinemaapp.backend.service.GenreService;
+import com.cinemaapp.backend.service.domain.model.Genre;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenreServiceImpl implements GenreService {
+
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreServiceImpl(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    @Override
+    public List<Genre> findAllGenres() {
+        return genreRepository.findAllGenres();
+    }
 }
