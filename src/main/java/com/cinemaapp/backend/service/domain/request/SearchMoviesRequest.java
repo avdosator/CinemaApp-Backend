6f69b31573310bb80b00fcd1127165f4ce796f50
Avdo Sator <avdo.sator@hotmail.com>
2024-11-03 19:46:17 +0100

    Add fields to SearchMoviesRequest that will be used for filtering

--- src/main/java/com/cinemaapp/backend/service/domain/request/SearchMoviesRequest.java
+++ src/main/java/com/cinemaapp/backend/service/domain/request/SearchMoviesRequest.java
@@ -1,9 +1,18 @@
 package com.cinemaapp.backend.service.domain.request;
 
+import java.time.LocalDate;
+
 public class SearchMoviesRequest {
 
     private int page = 0;
     private int size = 1000;
+    private String status = "";
+    private LocalDate date = null;
+    private String title = "";
+    private String city = "";
+    private String venue = "";
+    private String genre = "";
+    private String projectionTime = "";
 
     public int getPage() {
         return page;
@@ -19,5 +28,61 @@ public class SearchMoviesRequest {
 
     public void setSize(int size) {
         this.size = size;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getProjectionTime() {
+        return projectionTime;
+    }
+
+    public void setProjectionTime(String projectionTime) {
+        this.projectionTime = projectionTime;
     }
 }
