3ac3a0824acfeb980033d66ad5c66033dcc9544d
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 13:45:46 +0100

    Create endpoint for creating user

--- src/main/java/com/cinemaapp/backend/controller/UserController.java
+++ src/main/java/com/cinemaapp/backend/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.cinemaapp.backend.controller;
 
 import com.cinemaapp.backend.service.UserService;
+import com.cinemaapp.backend.service.domain.model.User;
+import com.cinemaapp.backend.service.domain.request.CreateUserRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +17,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
+        return userService.createUser(createUserRequest);
     }
 
 
