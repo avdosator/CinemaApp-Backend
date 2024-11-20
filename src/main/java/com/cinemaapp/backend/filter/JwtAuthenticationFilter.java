4f42ca38259ef58c3c5009cf6d8518b0f7851309
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 11:34:16 +0100

    Create JwtAuthenticationFilter that will handle incoming jwts

---
+++ src/main/java/com/cinemaapp/backend/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.cinemaapp.backend.filter;
+
+import com.cinemaapp.backend.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Autowired
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            UserDetailsService userDetailsService,
+            HandlerExceptionResolver handlerExceptionResolver
+    ) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+}
