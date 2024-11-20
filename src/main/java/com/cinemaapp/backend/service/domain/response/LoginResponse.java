6521318870daee036daa1647d87602b192c73e11
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 14:39:45 +0100

    Create LoginResponse class that will return jwt to user after successful login

---
+++ src/main/java/com/cinemaapp/backend/service/domain/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.cinemaapp.backend.service.domain.response;
+
+public class LoginResponse {
+
+    private String jwt;
+    private long expiresIn;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
