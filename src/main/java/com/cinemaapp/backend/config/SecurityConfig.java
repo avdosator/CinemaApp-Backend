84d427c4708b2c6140344729a636ebdd9351a7a9
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 11:20:20 +0100

    Create Bcrypt bean

--- src/main/java/com/cinemaapp/backend/config/SecurityConfig.java
+++ src/main/java/com/cinemaapp/backend/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,6 +25,11 @@ public class SecurityConfig {
             final User user = userRepository.findByEmail(email);
             return new CustomUserDetails(user);
         };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
