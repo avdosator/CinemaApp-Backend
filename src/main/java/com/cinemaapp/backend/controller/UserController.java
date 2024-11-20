f6358bb6660c2505c8f7e334c7e659db3715bb9a
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 15:02:50 +0100

    Add logic for authentication and generate jwt

--- src/main/java/com/cinemaapp/backend/controller/UserController.java
+++ src/main/java/com/cinemaapp/backend/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.cinemaapp.backend.controller;
 
+import com.cinemaapp.backend.service.JwtService;
 import com.cinemaapp.backend.service.UserService;
 import com.cinemaapp.backend.service.domain.model.User;
 import com.cinemaapp.backend.service.domain.request.CreateUserRequest;
+import com.cinemaapp.backend.service.domain.response.LoginResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +16,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService, UserDetailsService userDetailsService) {
         this.userService = userService;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/users")
     public User createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
         return userService.createUser(createUserRequest);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody CreateUserRequest createUserRequest) {
+        User authenticatedUser = userService.authenticate(createUserRequest);
+        String jwtToken = jwtService.generateToken(userDetailsService.loadUserByUsername(authenticatedUser.getEmail()));
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setJwt(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+        return loginResponse;
     }
 
 
