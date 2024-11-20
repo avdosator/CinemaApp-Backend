9a78be63f41fc17c125eda75d195b6f471e909d2
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 13:35:05 +0100

    Implement CreateUserRequest class that will represent body that comes in request when user is being created

--- src/main/java/com/cinemaapp/backend/controller/dto/CreateUserDto.java
+++
@@ -1,6 +0,0 @@
-package com.cinemaapp.backend.controller.dto;
-
-import com.cinemaapp.backend.service.domain.request.CreateUserRequest;
-
-public class CreateUserDto extends CreateUserRequest {
-}
--- src/main/java/com/cinemaapp/backend/service/domain/request/CreateUserRequest.java
+++ src/main/java/com/cinemaapp/backend/service/domain/request/CreateUserRequest.java
@@ -1,4 +1,37 @@
 package com.cinemaapp.backend.service.domain.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateUserRequest {
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Min(value = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    public CreateUserRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
