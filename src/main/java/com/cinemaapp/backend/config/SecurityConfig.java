58be7444a4479d5a2bc13d85a73b6de0d303b806
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 11:12:20 +0100

    Create bean that returns CustomUserDetails

--- src/main/java/com/cinemaapp/backend/config/SecurityConfig.java
+++ src/main/java/com/cinemaapp/backend/config/SecurityConfig.java
@@ -1,15 +1,29 @@
 package com.cinemaapp.backend.config;
 
+import com.cinemaapp.backend.config.security.CustomUserDetails;
+import com.cinemaapp.backend.repository.UserRepository;
+import com.cinemaapp.backend.service.domain.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            final User user = userRepository.findByUsername(username);
+            return new CustomUserDetails(user);
+        };
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
