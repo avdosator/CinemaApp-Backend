e4e245ec9eaefbb9098ad9663a5ad7e771c7e700
Avdo Sator <avdo.sator@hotmail.com>
2024-11-20 20:52:01 +0100

    Implement method that creates refresh token

---
+++ src/main/java/com/cinemaapp/backend/repository/impl/RefreshTokenJpaRepository.java
@@ -0,0 +1,62 @@
+package com.cinemaapp.backend.repository.impl;
+
+import com.cinemaapp.backend.repository.RefreshTokenRepository;
+import com.cinemaapp.backend.repository.crud.CrudRefreshTokenRepository;
+import com.cinemaapp.backend.repository.crud.CrudUserRepository;
+import com.cinemaapp.backend.repository.entity.RefreshTokenEntity;
+import com.cinemaapp.backend.service.domain.model.RefreshToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.UUID;
+
+@Repository
+public class RefreshTokenJpaRepository implements RefreshTokenRepository {
+
+    private static final int TOKEN_LENGTH = 64;
+    private static final int TOKEN_DURATION = 14; //days
+
+    private final CrudRefreshTokenRepository crudRefreshTokenRepository;
+    private final CrudUserRepository crudUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RefreshTokenJpaRepository(CrudRefreshTokenRepository crudRefreshTokenRepository,
+                                     CrudUserRepository crudUserRepository,
+                                     PasswordEncoder passwordEncoder) {
+        this.crudRefreshTokenRepository = crudRefreshTokenRepository;
+        this.crudUserRepository = crudUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public String createRefreshToken(UUID userId) {
+        byte[] randomBytes = new byte[TOKEN_LENGTH];
+        new SecureRandom().nextBytes(randomBytes);
+        String token = Base64.getUrlEncoder().encodeToString(randomBytes);
+        String tokenHash = passwordEncoder.encode(token);
+
+        RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity();
+        refreshTokenEntity.setTokenHash(tokenHash);
+        refreshTokenEntity.setUserEntity(crudUserRepository.findById(userId).orElseThrow());
+        refreshTokenEntity.setExpiration(LocalDateTime.now().plusDays(TOKEN_DURATION));
+        refreshTokenEntity.setCreatedAt(LocalDateTime.now());
+        RefreshTokenEntity createdRefreshTokenEntity = crudRefreshTokenRepository.save(refreshTokenEntity);
+
+        return createdRefreshTokenEntity.getTokenHash();
+    }
+
+    @Override
+    public RefreshToken validateToken(String token) {
+        return null;
+    }
+
+    @Override
+    public void deleteToken(String token) {
+
+    }
+}
