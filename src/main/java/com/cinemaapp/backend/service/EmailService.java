31e1e359eeaa74282d9cc07f88c33af2ed6f3d11
Avdo Sator <avdo.sator@hotmail.com>
2024-11-23 14:31:42 +0100

    Create EmailService

---
+++ src/main/java/com/cinemaapp/backend/service/EmailService.java
@@ -0,0 +1,32 @@
+package com.cinemaapp.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.mail.username}")
+    private String emailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(emailSender);
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+}
