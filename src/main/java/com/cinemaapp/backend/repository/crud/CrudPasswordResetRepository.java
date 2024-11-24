217bcae3816f1170acdf68fd3c5d0b12fe2e3ca6
Avdo Sator <avdo.sator@hotmail.com>
2024-11-24 12:33:59 +0100

    Implement cron job that will delete all expired tokes every 24 hours

--- src/main/java/com/cinemaapp/backend/repository/crud/CrudPasswordResetRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/crud/CrudPasswordResetRepository.java
@@ -2,13 +2,18 @@ package com.cinemaapp.backend.repository.crud;
 
 import com.cinemaapp.backend.repository.entity.PasswordResetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CrudPasswordResetRepository extends JpaRepository<PasswordResetEntity, UUID> {
 
-    Optional<PasswordResetEntity> findByUserId(UUID userId);
     Optional<PasswordResetEntity> findByResetCodeAndUserId(String resetCode, UUID userId);
-    void deleteByUserId(UUID userId);
+
+    @Modifying
+    @Transactional
+    int deleteByExpirationTimeBefore(LocalDateTime now);
 }
