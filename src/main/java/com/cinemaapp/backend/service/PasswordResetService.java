fffc7a53f068ef5f510e07b623ab88063a16c6a9
Avdo Sator <avdo.sator@hotmail.com>
2024-11-24 11:22:02 +0100

    Add comment for possible solution

--- src/main/java/com/cinemaapp/backend/service/PasswordResetService.java
+++ src/main/java/com/cinemaapp/backend/service/PasswordResetService.java
@@ -1,6 +1,9 @@
 package com.cinemaapp.backend.service;
 
+import com.cinemaapp.backend.service.domain.request.VerifyResetCodeRequest;
+
 public interface PasswordResetService {
 
     String initiatePasswordReset(String email);
+    boolean verifyResetCode(VerifyResetCodeRequest verifyResetCodeRequest);
 }
