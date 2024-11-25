f83c9988b7e29107844539d5b10bca9ecdfcf254
Avdo Sator <avdo.sator@hotmail.com>
2024-11-25 10:54:58 +0100

    Create AuthService that will handle generating LoginResponse

---
+++ src/main/java/com/cinemaapp/backend/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.cinemaapp.backend.service;
+
+import com.cinemaapp.backend.service.domain.model.User;
+import com.cinemaapp.backend.service.domain.response.LoginResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
+    private final UserDetailsService userDetailsService;
+
+    @Autowired
+    public AuthService(JwtService jwtService,
+                       RefreshTokenService refreshTokenService,
+                       UserDetailsService userDetailsService
+    ) {
+        this.jwtService = jwtService;
+        this.refreshTokenService = refreshTokenService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    public LoginResponse authenticateAndLogin(User authenticatedUser) {
+        String jwtToken = jwtService.generateToken(userDetailsService.loadUserByUsername(authenticatedUser.getEmail()));
+        String refreshToken = refreshTokenService.createRefreshToken(authenticatedUser.getId());
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setUser(authenticatedUser);
+        loginResponse.setJwt(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+        loginResponse.setRefreshToken(refreshToken);
+
+        return loginResponse;
+    }
+}
