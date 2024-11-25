4906936764190571593a805dd94ebd3a07436c2e
Avdo Sator <avdo.sator@hotmail.com>
2024-11-25 08:41:02 +0100

    Create ChangePasswordRequest class

---
+++ src/main/java/com/cinemaapp/backend/service/domain/request/ChangePasswordRequest.java
@@ -0,0 +1,31 @@
+package com.cinemaapp.backend.service.domain.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ChangePasswordRequest {
+
+    @Email
+    private String email;
+
+    @NotBlank(message = "Password can't be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String newPassword;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
