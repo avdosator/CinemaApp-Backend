5342255eed2382323d758c03989a11f53906ba12
Avdo Sator <avdo.sator@hotmail.com>
2024-12-02 13:59:56 +0100

    Split AuthService to interface and implementation

--- src/main/java/com/cinemaapp/backend/service/impl/AuthServiceImpl.java
+++ src/main/java/com/cinemaapp/backend/service/impl/AuthServiceImpl.java
@@ -1,40 +1,41 @@
-package com.cinemaapp.backend.service;
+package com.cinemaapp.backend.service.impl;
 
+import com.cinemaapp.backend.service.AuthService;
+import com.cinemaapp.backend.service.JwtService;
+import com.cinemaapp.backend.service.RefreshTokenService;
 import com.cinemaapp.backend.service.domain.model.User;
-import com.cinemaapp.backend.service.domain.response.LoginResponse;
-import com.cinemaapp.backend.service.domain.response.SignUpResponse;
+import com.cinemaapp.backend.service.domain.response.auth.LoginResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthService {
+public class AuthServiceImpl implements AuthService {
 
     private final JwtService jwtService;
     private final RefreshTokenService refreshTokenService;
     private final UserDetailsService userDetailsService;
 
     @Autowired
-    public AuthService(JwtService jwtService,
-                       RefreshTokenService refreshTokenService,
-                       UserDetailsService userDetailsService
+    public AuthServiceImpl(JwtService jwtService,
+                           RefreshTokenService refreshTokenService,
+                           UserDetailsService userDetailsService
     ) {
         this.jwtService = jwtService;
         this.refreshTokenService = refreshTokenService;
         this.userDetailsService = userDetailsService;
     }
 
-    public LoginResponse authenticateAndLogin(User authenticatedUser) {
+    @Override
+    public LoginResponse authenticateAndLogin(User authenticatedUser, boolean isRememberMe) {
         String jwtToken = jwtService.generateToken(userDetailsService.loadUserByUsername(authenticatedUser.getEmail()));
-        String refreshToken = refreshTokenService.createRefreshToken(authenticatedUser.getId());
-
-        SignUpResponse signUpResponse ) new SignUpResponse()
         LoginResponse loginResponse = new LoginResponse();
-        loginResponse.setUser(authenticatedUser);
         loginResponse.setJwt(jwtToken);
         loginResponse.setExpiresIn(jwtService.getExpirationTime());
-        loginResponse.setRefreshToken(refreshToken);
-
+        if (isRememberMe) {
+            String refreshToken = refreshTokenService.createRefreshToken(authenticatedUser.getId());
+            loginResponse.setRefreshToken(refreshToken);
+        }
         return loginResponse;
     }
 }
