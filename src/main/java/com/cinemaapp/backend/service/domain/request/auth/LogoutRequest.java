ffd7c2b4931a17201b9e6707ba65ca2c4ccac3b7
Avdo Sator <avdo.sator@hotmail.com>
2024-12-05 18:55:17 +0100

    Add userId field

--- src/main/java/com/cinemaapp/backend/service/domain/request/auth/LogoutRequest.java
+++ src/main/java/com/cinemaapp/backend/service/domain/request/auth/LogoutRequest.java
@@ -2,9 +2,13 @@ package com.cinemaapp.backend.service.domain.request.auth;
 
 import jakarta.validation.constraints.Size;
 
+import java.util.UUID;
+
 public class LogoutRequest {
     @Size(min = 86, max = 86)
     private String refreshToken;
+
+    private UUID userId;
 
     public String getRefreshToken() {
         return refreshToken;
@@ -12,5 +16,13 @@ public class LogoutRequest {
 
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
     }
 }
