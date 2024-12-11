f4d7e1bd529dd7fe05c4349579a80ed24112c7c5
Avdo Sator <avdo.sator@hotmail.com>
2024-12-11 14:00:51 +0100

    Add endpoint that will return specific projection instance based on query params

--- src/main/java/com/cinemaapp/backend/controller/ProjectionInstanceController.java
+++ src/main/java/com/cinemaapp/backend/controller/ProjectionInstanceController.java
@@ -1,13 +1,18 @@
 package com.cinemaapp.backend.controller;
 
 import com.cinemaapp.backend.service.ProjectionInstanceService;
+import com.cinemaapp.backend.service.domain.model.ProjectionInstance;
+import com.cinemaapp.backend.service.domain.request.SearchProjectionInstanceRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/projection/instances")
+@RequestMapping("/projection/instance")
 @Tag(name = "Projection instances", description = "Endpoints for managing projection instances")
 public class ProjectionInstanceController {
 
@@ -16,5 +21,10 @@ public class ProjectionInstanceController {
     @Autowired
     public ProjectionInstanceController(ProjectionInstanceService projectionInstanceService) {
         this.projectionInstanceService = projectionInstanceService;
+    }
+
+    @GetMapping()
+    public ProjectionInstance getProjectionInstance(@ModelAttribute @Valid SearchProjectionInstanceRequest searchProjectionInstanceRequest) {
+        return projectionInstanceService.findProjectionInstance(searchProjectionInstanceRequest);
     }
 }
