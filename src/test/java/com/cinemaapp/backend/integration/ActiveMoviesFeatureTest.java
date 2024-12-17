b28281ea1283051d6580a2def56d8c3d477d1d61
Avdo Sator <avdo.sator@hotmail.com>
2024-12-17 22:46:28 +0100

    Add MockMvc dependency for endpoint testing

--- src/test/java/com/cinemaapp/backend/integration/ActiveMoviesFeatureTest.java
+++ src/test/java/com/cinemaapp/backend/integration/ActiveMoviesFeatureTest.java
@@ -1,11 +1,20 @@
 package com.cinemaapp.backend.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class ActiveMoviesFeatureTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
 }
