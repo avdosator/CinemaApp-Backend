ba222e0defcd5bfeee1453153f2e25618a7b7072
Avdo Sator <avdo.sator@hotmail.com>
2024-12-17 22:35:50 +0100

    Create MovieRepository mock

--- src/test/java/com/cinemaapp/backend/service/impl/MovieServiceImplTest.java
+++ src/test/java/com/cinemaapp/backend/service/impl/MovieServiceImplTest.java
@@ -1,9 +1,22 @@
 package com.cinemaapp.backend.service.impl;
 
+import com.cinemaapp.backend.repository.MovieRepository;
+import com.cinemaapp.backend.service.MovieService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
 @ExtendWith(MockitoExtension.class)
 public class MovieServiceImplTest {
+
+    @Mock
+    private MovieRepository movieRepository;
+    private MovieService movieService;
+
+    @BeforeEach
+    public void setUp() {
+        movieService = new MovieServiceImpl(movieRepository);
+    }
 }
