2b676dcc2be3c1abd68c69e4b221d249aa32eb29
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 14:24:37 +0100

    Add endpoint for creating payment

--- src/main/java/com/cinemaapp/backend/controller/PaymentController.java
+++ src/main/java/com/cinemaapp/backend/controller/PaymentController.java
@@ -1,7 +1,28 @@
 package com.cinemaapp.backend.controller;
 
+import com.cinemaapp.backend.service.PaymentService;
+import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/payments")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public String createPayment(@Valid @RequestBody CreatePaymentRequest createPaymentRequest) {
+        paymentService.createPayment(createPaymentRequest);
+        return "";
+    }
 }
