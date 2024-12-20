cf942397d4ccf599c92fd2123ebdee9ce45f64a7
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 20:39:43 +0100

    Create payment response class

---
+++ src/main/java/com/cinemaapp/backend/service/domain/response/CreatePaymentResponse.java
@@ -0,0 +1,34 @@
+package com.cinemaapp.backend.service.domain.response;
+
+import java.util.UUID;
+
+public class CreatePaymentResponse {
+
+    private UUID reservationId;
+    private String paymentStatus;
+    private double amount;
+
+    public UUID getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(UUID reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
