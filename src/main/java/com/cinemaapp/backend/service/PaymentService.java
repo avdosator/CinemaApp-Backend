b38405729056dd14e7fa8faf640c13b06a170073
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 14:32:20 +0100

    Add method for creating payment

--- src/main/java/com/cinemaapp/backend/service/PaymentService.java
+++ src/main/java/com/cinemaapp/backend/service/PaymentService.java
@@ -1,4 +1,8 @@
 package com.cinemaapp.backend.service;
 
+import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
+
 public interface PaymentService {
+
+    public String createPayment(CreatePaymentRequest createPaymentRequest);
 }
--- src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,26 @@
 package com.cinemaapp.backend.service.impl;
 
+import com.cinemaapp.backend.repository.PaymentRepository;
 import com.cinemaapp.backend.service.PaymentService;
+import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    @Autowired
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Transactional
+    @Override
+    public String createPayment(CreatePaymentRequest createPaymentRequest) {
+        paymentRepository.createPayment(createPaymentRequest);
+        return "";
+    }
 }
