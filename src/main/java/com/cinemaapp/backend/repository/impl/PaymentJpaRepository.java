aaf12896f6622c0c625e6a5d6935f7d0e6215895
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 14:48:20 +0100

    Add method for creating reservation

--- src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
@@ -2,25 +2,51 @@ package com.cinemaapp.backend.repository.impl;
 
 import com.cinemaapp.backend.repository.PaymentRepository;
 import com.cinemaapp.backend.repository.crud.CrudPaymentRepository;
+import com.cinemaapp.backend.repository.crud.CrudReservationRepository;
 import com.cinemaapp.backend.repository.crud.CrudUserRepository;
+import com.cinemaapp.backend.repository.entity.ReservationEntity;
+import com.cinemaapp.backend.repository.entity.SeatReservationEntity;
+import com.cinemaapp.backend.repository.entity.UserEntity;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public class PaymentJpaRepository implements PaymentRepository {
 
     private final CrudPaymentRepository crudPaymentRepository;
     private final CrudUserRepository crudUserRepository;
+    private final CrudReservationRepository crudReservationRepository;
 
     @Autowired
-    public PaymentJpaRepository(CrudPaymentRepository crudPaymentRepository, CrudUserRepository crudUserRepository) {
+    public PaymentJpaRepository(CrudPaymentRepository crudPaymentRepository, CrudUserRepository crudUserRepository,
+                                CrudReservationRepository crudReservationRepository) {
         this.crudPaymentRepository = crudPaymentRepository;
         this.crudUserRepository = crudUserRepository;
+        this.crudReservationRepository = crudReservationRepository;
     }
 
     @Override
     public String createPayment(CreatePaymentRequest createPaymentRequest) {
+
+        UserEntity userEntity = crudUserRepository.findById(createPaymentRequest.getUserId()).orElseThrow();
+        ReservationEntity reservationEntity = createReservation(createPaymentRequest, userEntity);
         return "";
     }
+
+    private ReservationEntity createReservation(CreatePaymentRequest createPaymentRequest, UserEntity userEntity) {
+
+        ReservationEntity reservationEntity = new ReservationEntity();
+        reservationEntity.setUserEntity(userEntity);
+        reservationEntity.setStatus("confirmed");
+        reservationEntity.setTotalPrice(createPaymentRequest.getAmount());
+        reservationEntity.setSeatReservationEntities(createSeatReservations());
+        reservationEntity.setCreatedAt(LocalDateTime.now());
+        return crudReservationRepository.save(reservationEntity);
+    }
+
+    
 }
