a0d47e0744af3d9ba5186caa14ba6f83ce0ff459
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 20:23:23 +0100

    Add method for creating seat reservations from request

--- src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
@@ -1,18 +1,16 @@
 package com.cinemaapp.backend.repository.impl;
 
 import com.cinemaapp.backend.repository.PaymentRepository;
-import com.cinemaapp.backend.repository.crud.CrudPaymentRepository;
-import com.cinemaapp.backend.repository.crud.CrudReservationRepository;
-import com.cinemaapp.backend.repository.crud.CrudUserRepository;
-import com.cinemaapp.backend.repository.entity.ReservationEntity;
-import com.cinemaapp.backend.repository.entity.SeatReservationEntity;
-import com.cinemaapp.backend.repository.entity.UserEntity;
+import com.cinemaapp.backend.repository.crud.*;
+import com.cinemaapp.backend.repository.entity.*;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class PaymentJpaRepository implements PaymentRepository {
@@ -20,13 +18,21 @@ public class PaymentJpaRepository implements PaymentRepository {
     private final CrudPaymentRepository crudPaymentRepository;
     private final CrudUserRepository crudUserRepository;
     private final CrudReservationRepository crudReservationRepository;
+    private final CrudSeatRepository crudSeatRepository;
+    private final CrudSeatReservationRepository crudSeatReservationRepository;
+    private final CrudProjectionInstanceRepository crudProjectionInstanceRepository;
 
     @Autowired
     public PaymentJpaRepository(CrudPaymentRepository crudPaymentRepository, CrudUserRepository crudUserRepository,
-                                CrudReservationRepository crudReservationRepository) {
+                                CrudReservationRepository crudReservationRepository, CrudSeatRepository crudSeatRepository,
+                                CrudSeatReservationRepository crudSeatReservationRepository,
+                                CrudProjectionInstanceRepository crudProjectionInstanceRepository) {
         this.crudPaymentRepository = crudPaymentRepository;
         this.crudUserRepository = crudUserRepository;
         this.crudReservationRepository = crudReservationRepository;
+        this.crudSeatRepository = crudSeatRepository;
+        this.crudSeatReservationRepository = crudSeatReservationRepository;
+        this.crudProjectionInstanceRepository = crudProjectionInstanceRepository;
     }
 
     @Override
@@ -43,10 +49,24 @@ public class PaymentJpaRepository implements PaymentRepository {
         reservationEntity.setUserEntity(userEntity);
         reservationEntity.setStatus("confirmed");
         reservationEntity.setTotalPrice(createPaymentRequest.getAmount());
-        reservationEntity.setSeatReservationEntities(createSeatReservations());
+        reservationEntity.setSeatReservationEntities(createSeatReservations(createPaymentRequest, userEntity));
         reservationEntity.setCreatedAt(LocalDateTime.now());
         return crudReservationRepository.save(reservationEntity);
     }
 
-    
+    private List<SeatReservationEntity> createSeatReservations(CreatePaymentRequest createPaymentRequest, UserEntity userEntity) {
+        ProjectionInstanceEntity projectionInstanceEntity = crudProjectionInstanceRepository
+                .findById(createPaymentRequest.getProjectionInstanceId()).orElseThrow();
+        List<SeatReservationEntity> seatReservationEntities = new ArrayList<>();
+        for (UUID seatId : createPaymentRequest.getSelectedSeats()) {
+            SeatReservationEntity seatReservationEntity = new SeatReservationEntity();
+            seatReservationEntity.setProjectionInstanceEntity(projectionInstanceEntity);
+            seatReservationEntity.setSeatEntity(crudSeatRepository.findById(seatId).orElseThrow());
+            seatReservationEntity.setStatus("purchased");
+            seatReservationEntity.setCreatedAt(LocalDateTime.now());
+            seatReservationEntity.setUserEntity(userEntity);
+            seatReservationEntities.add(crudSeatReservationRepository.save(seatReservationEntity));
+        }
+        return seatReservationEntities;
+    }
 }
