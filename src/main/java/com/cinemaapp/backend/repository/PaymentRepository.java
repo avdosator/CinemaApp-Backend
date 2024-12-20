face89df55a6550359bdb7fd2428fc28d3afa4d8
Avdo Sator <avdo.sator@hotmail.com>
2024-12-20 14:36:22 +0100

    Add createPayment method

--- src/main/java/com/cinemaapp/backend/repository/PaymentRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/PaymentRepository.java
@@ -1,4 +1,8 @@
 package com.cinemaapp.backend.repository;
 
+import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
+
 public interface PaymentRepository {
+
+    public String createPayment(CreatePaymentRequest createPaymentRequest);
 }
--- src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/impl/PaymentJpaRepository.java
@@ -1,8 +1,26 @@
 package com.cinemaapp.backend.repository.impl;
 
 import com.cinemaapp.backend.repository.PaymentRepository;
+import com.cinemaapp.backend.repository.crud.CrudPaymentRepository;
+import com.cinemaapp.backend.repository.crud.CrudUserRepository;
+import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PaymentJpaRepository implements PaymentRepository {
+
+    private final CrudPaymentRepository crudPaymentRepository;
+    private final CrudUserRepository crudUserRepository;
+
+    @Autowired
+    public PaymentJpaRepository(CrudPaymentRepository crudPaymentRepository, CrudUserRepository crudUserRepository) {
+        this.crudPaymentRepository = crudPaymentRepository;
+        this.crudUserRepository = crudUserRepository;
+    }
+
+    @Override
+    public String createPayment(CreatePaymentRequest createPaymentRequest) {
+        return "";
+    }
 }
