109d95b01a77d45c29272e1103310e56d0a1cb6f
Avdo Sator <avdo.sator@hotmail.com>
2024-12-22 19:57:19 +0100

    Update creation of payment intent

--- src/main/java/com/cinemaapp/backend/service/StripeService.java
+++ src/main/java/com/cinemaapp/backend/service/StripeService.java
@@ -4,18 +4,22 @@ import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class StripeService {
 
-    public PaymentIntent createPaymentIntent(double amount, String currency) throws Exception {
+    public PaymentIntent createPaymentIntent(double amount, UUID userId, UUID projectionInstanceId ) throws Exception {
         PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setAmount((long) (amount * 100)) // Amount in cents
-                .setCurrency(currency)
+                .setCurrency("EUR")
                 .setAutomaticPaymentMethods(
                         PaymentIntentCreateParams.AutomaticPaymentMethods.builder()
                                 .setEnabled(true)
                                 .build()
                 )
+                .putMetadata("userId", userId.toString())
+                .putMetadata("projectionInstanceId", projectionInstanceId.toString())
                 .build();
 
         return PaymentIntent.create(params);
