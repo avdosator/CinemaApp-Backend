73503668080e6f1f52aefba63db993b44f178199
Avdo Sator <avdo.sator@hotmail.com>
2024-12-22 20:25:55 +0100

    Implement methods for creating payment and payment intent(wip)

--- src/main/java/com/cinemaapp/backend/service/PaymentService.java
+++ src/main/java/com/cinemaapp/backend/service/PaymentService.java
@@ -1,9 +1,12 @@
 package com.cinemaapp.backend.service;
 
+import com.cinemaapp.backend.service.domain.request.CreatePaymentIntentRequest;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
 import com.cinemaapp.backend.service.domain.response.CreatePaymentResponse;
 
 public interface PaymentService {
 
-    public CreatePaymentResponse createPayment(CreatePaymentRequest createPaymentRequest);
+    CreatePaymentResponse createPayment(CreatePaymentRequest createPaymentRequest);
+    String createPaymentIntent(CreatePaymentIntentRequest createPaymentIntentRequest);
+    //void handleFailedPayment(UUID paymentId);
 }
--- src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
@@ -1,32 +1,55 @@
 package com.cinemaapp.backend.service.impl;
 
+import com.cinemaapp.backend.exception.PaymentProcessingException;
 import com.cinemaapp.backend.repository.PaymentRepository;
 import com.cinemaapp.backend.service.PaymentService;
-import com.cinemaapp.backend.service.domain.model.Reservation;
+import com.cinemaapp.backend.service.StripeService;
+import com.cinemaapp.backend.service.domain.request.CreatePaymentIntentRequest;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
 import com.cinemaapp.backend.service.domain.response.CreatePaymentResponse;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
+    private final StripeService stripeService;
 
     @Autowired
-    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+    public PaymentServiceImpl(PaymentRepository paymentRepository, StripeService stripeService) {
         this.paymentRepository = paymentRepository;
+        this.stripeService = stripeService;
     }
 
     @Transactional
     @Override
     public CreatePaymentResponse createPayment(CreatePaymentRequest createPaymentRequest) {
-        Reservation reservation =  paymentRepository.createPayment(createPaymentRequest);
-        CreatePaymentResponse createPaymentResponse = new CreatePaymentResponse();
-        createPaymentResponse.setReservationId(reservation.getId());
-        createPaymentResponse.setPaymentStatus("pending");
-        createPaymentResponse.setTotalPrice(reservation.getTotalPrice());
-        return createPaymentResponse;
+
+        // Step 1: Retrieve and validate PaymentIntent from Stripe
+        PaymentIntent paymentIntent = stripeService.retrievePaymentIntent(createPaymentRequest.getPaymentIntentId());
+        if (!"succeeded".equals(paymentIntent.getStatus())) {
+            throw new PaymentProcessingException("Payment was not successful");
+        }
+
+        // Step 2: Create all entities (reservation, seat reservation(s), payment and ticket)
+        UUID reservationId = paymentRepository.createEntities(createPaymentRequest);
+    }
+
+    @Override
+    public String createPaymentIntent(CreatePaymentIntentRequest createPaymentIntentRequest) {
+        try {
+            PaymentIntent paymentIntent = stripeService.createPaymentIntent(
+                    createPaymentIntentRequest.getAmount(),
+                    createPaymentIntentRequest.getUserId(),
+                    createPaymentIntentRequest.getProjectionInstanceId());
+            return paymentIntent.getClientSecret();
+        } catch (Exception e) {
+            throw new PaymentProcessingException("Failed to create payment with Stripe", e);
+        }
     }
 }
