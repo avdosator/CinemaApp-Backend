f87c3e263a1d11cb1a43c80f40642d39a43672c9
Avdo Sator <avdo.sator@hotmail.com>
2024-12-22 20:25:21 +0100

    Implement method for retrieving PaymentIntent

--- src/main/java/com/cinemaapp/backend/service/StripeService.java
+++ src/main/java/com/cinemaapp/backend/service/StripeService.java
@@ -1,5 +1,6 @@
 package com.cinemaapp.backend.service;
 
+import com.cinemaapp.backend.exception.PaymentProcessingException;
 import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import java.util.UUID;
 @Service
 public class StripeService {
 
-    public PaymentIntent createPaymentIntent(double amount, UUID userId, UUID projectionInstanceId ) throws Exception {
+    public PaymentIntent createPaymentIntent(double amount, UUID userId, UUID projectionInstanceId) throws Exception {
         PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setAmount((long) (amount * 100)) // Amount in cents
                 .setCurrency("EUR")
@@ -23,5 +24,13 @@ public class StripeService {
                 .build();
 
         return PaymentIntent.create(params);
+    }
+
+    public PaymentIntent retrievePaymentIntent(String paymentIntentId) {
+        try {
+            return PaymentIntent.retrieve(paymentIntentId);
+        } catch (Exception e) {
+            throw new PaymentProcessingException("Failed to retrieve PaymentIntent with ID: " + paymentIntentId, e);
+        }
     }
 }
