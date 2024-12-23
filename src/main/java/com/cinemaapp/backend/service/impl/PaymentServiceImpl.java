065e89708ffe4b8d04db7416c977d8de23d01739
Avdo Sator <avdo.sator@hotmail.com>
2024-12-23 11:06:41 +0100

    Use pdfService to generate ticket in pdf format

--- src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/cinemaapp/backend/service/impl/PaymentServiceImpl.java
@@ -3,8 +3,12 @@ package com.cinemaapp.backend.service.impl;
 import com.cinemaapp.backend.controller.dto.PaymentProcessingResult;
 import com.cinemaapp.backend.exception.PaymentProcessingException;
 import com.cinemaapp.backend.repository.PaymentRepository;
+import com.cinemaapp.backend.service.MovieService;
 import com.cinemaapp.backend.service.PaymentService;
+import com.cinemaapp.backend.service.PdfService;
 import com.cinemaapp.backend.service.StripeService;
+import com.cinemaapp.backend.service.domain.model.Movie;
+import com.cinemaapp.backend.service.domain.model.ProjectionInstance;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentIntentRequest;
 import com.cinemaapp.backend.service.domain.request.CreatePaymentRequest;
 import com.cinemaapp.backend.service.domain.response.PaymentConfirmationResponse;
@@ -13,16 +17,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final StripeService stripeService;
+    private final PdfService pdfService;
+    private final MovieService movieService;
 
     @Autowired
-    public PaymentServiceImpl(PaymentRepository paymentRepository, StripeService stripeService) {
+    public PaymentServiceImpl(PaymentRepository paymentRepository, StripeService stripeService, PdfService pdfService,
+                              MovieService movieService) {
         this.paymentRepository = paymentRepository;
         this.stripeService = stripeService;
+        this.pdfService = pdfService;
+        this.movieService = movieService;
     }
 
     @Transactional
@@ -37,6 +48,28 @@ public class PaymentServiceImpl implements PaymentService {
 
         // Step 2: Create all entities (reservation, seat reservation(s), payment and ticket)
         PaymentProcessingResult paymentProcessingResult = paymentRepository.processReservationAndPayment(createPaymentRequest);
+
+        // Step 3: Generate ticket PDF
+        Movie movie = movieService.findById(paymentProcessingResult.getReservation().getSeatReservations().get(0).getProjectionInstance().getProjection().getMovieId());
+        ProjectionInstance projectionInstance = paymentProcessingResult.getReservation().getSeatReservations().get(0).getProjectionInstance();
+        List<String> seatNumbers = paymentProcessingResult.getReservation()
+                .getSeatReservations()
+                .stream()
+                .map(seatReservation -> seatReservation.getSeat().getNumber())
+                .toList();
+
+        byte[] ticketPdf = pdfService.generateTicket(
+                movie.getTitle(),
+                projectionInstance.getDate().toString(),
+                projectionInstance.getTime(),
+                projectionInstance.getProjection().getHall().getVenue().getName(),
+                projectionInstance.getProjection().getHall().getName(),
+                seatNumbers,
+                paymentProcessingResult.getPayment().getAmount(),
+                movie.getPgRating(),
+                movie.getLanguage(),
+                movie.getDurationInMinutes()
+        );
 
         return new PaymentConfirmationResponse("success", "Reservation and payment successfully processed");
     }
