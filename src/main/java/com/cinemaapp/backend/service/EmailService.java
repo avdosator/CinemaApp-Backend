5e3466f2b5efbda5c43ba8b7e85fbc0044b1cda0
Avdo Sator <avdo.sator@hotmail.com>
2024-12-23 11:35:41 +0100

    Implement sending receipt and ticket on email

--- src/main/java/com/cinemaapp/backend/service/EmailService.java
+++ src/main/java/com/cinemaapp/backend/service/EmailService.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -22,7 +23,7 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public String sendEmail(String to, String subject, String body) {
+    public String sendResetCode(String to, String subject, String body) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
@@ -32,6 +33,32 @@ public class EmailService {
             helper.setText(body, true);
             javaMailSender.send(mimeMessage);
             return "Email sent!";
+        } catch (MailException | MessagingException e) {
+            return e.getMessage();
+        }
+    }
+
+    public String sendTicketAndReceipt(String to, String subject, String body, byte[] ticketPdf, byte[] receiptPdf) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true); // Enable multipart for attachments
+            helper.setFrom(emailSender);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+
+            // Attach Ticket PDF
+            if (ticketPdf != null) {
+                helper.addAttachment("Ticket.pdf", new ByteArrayResource(ticketPdf));
+            }
+
+            // Attach Receipt PDF
+            if (receiptPdf != null) {
+                helper.addAttachment("Receipt.pdf", new ByteArrayResource(receiptPdf));
+            }
+
+            javaMailSender.send(mimeMessage);
+            return "Email with attachments sent!";
         } catch (MailException | MessagingException e) {
             return e.getMessage();
         }
