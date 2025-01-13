c90d04cd85b6d2deace32c337036f367bb0835e6
Avdo Sator <avdo.sator@hotmail.com>
2025-01-13 12:44:30 +0100

    Create TicketPriceEntity class

---
+++ src/main/java/com/cinemaapp/backend/repository/entity/TicketPriceEntity.java
@@ -0,0 +1,71 @@
+package com.cinemaapp.backend.repository.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ticket_price")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class TicketPriceEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "seat_type", nullable = false)
+    private String seatType;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
