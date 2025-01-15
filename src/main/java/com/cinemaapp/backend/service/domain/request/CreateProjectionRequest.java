240855f0c68808d34951966c6ea2a762b7dbe601
Avdo Sator <avdo.sator@hotmail.com>
2025-01-15 16:06:50 +0100

    Create CreateProjectionRequest dto class

--- src/main/java/com/cinemaapp/backend/service/domain/request/CreateProjectionRequest.java
+++ src/main/java/com/cinemaapp/backend/service/domain/request/CreateProjectionRequest.java
@@ -1,4 +1,18 @@
 package com.cinemaapp.backend.service.domain.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public class CreateProjectionRequest {
+
+    @NotEmpty
+    private String projectionTime;
+
+    @NotNull
+    private UUID cityId;
+
+    @NotNull
+    private UUID venueId;
 }
