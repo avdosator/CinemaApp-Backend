6d4835df7b649b7e2f49a32f4d573fa01921afe2
Avdo Sator <avdo.sator@hotmail.com>
2025-01-27 20:52:13 +0100

    Create endpoint for creating venue

--- src/main/java/com/cinemaapp/backend/controller/VenueController.java
+++ src/main/java/com/cinemaapp/backend/controller/VenueController.java
@@ -2,11 +2,17 @@ package com.cinemaapp.backend.controller;
 
 import com.cinemaapp.backend.controller.dto.Page;
 import com.cinemaapp.backend.service.VenueService;
+import com.cinemaapp.backend.service.domain.model.Movie;
 import com.cinemaapp.backend.service.domain.model.Venue;
+import com.cinemaapp.backend.service.domain.request.CreateMovieRequest;
+import com.cinemaapp.backend.service.domain.request.CreateVenueRequest;
 import com.cinemaapp.backend.service.domain.request.SearchVenuesRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,5 +32,14 @@ public class VenueController {
     @GetMapping
     public Page<Venue> getVenues(@ModelAttribute SearchVenuesRequest searchVenuesRequest) {
         return venueService.findVenues(searchVenuesRequest);
+    }
+
+    @Operation(summary = "Create venue", description = "Create new venue.")
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public Venue createVenue(
+            @Parameter(description = "Data needed for movie creation")
+            @RequestBody @Valid CreateVenueRequest createVenueRequest) {
+        return venueService.createVenue(createVenueRequest);
     }
 }
