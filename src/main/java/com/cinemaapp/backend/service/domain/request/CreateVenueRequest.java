4935bd0f293f06545d39dedd0f783df13c653fc2
Avdo Sator <avdo.sator@hotmail.com>
2025-01-27 20:45:26 +0100

    Add fields to CreateVenueRequest class

--- src/main/java/com/cinemaapp/backend/service/domain/request/CreateVenueRequest.java
+++ src/main/java/com/cinemaapp/backend/service/domain/request/CreateVenueRequest.java
@@ -1,4 +1,74 @@
 package com.cinemaapp.backend.service.domain.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public class CreateVenueRequest {
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String phone;
+
+    @NotEmpty
+    private String street;
+
+    @NotEmpty
+    private String streetNumber;
+
+    @NotNull
+    private UUID cityId;
+
+    private String photoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public UUID getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(UUID cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
