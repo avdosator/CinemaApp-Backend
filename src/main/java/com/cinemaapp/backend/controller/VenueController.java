75b1abf053056c0e2e1764fe6054fff983dbdc7f
Avdo Sator <avdo.sator@hotmail.com>
2025-01-28 14:23:42 +0100

    Create endpoint for updating venue

--- src/main/java/com/cinemaapp/backend/controller/VenueController.java
+++ src/main/java/com/cinemaapp/backend/controller/VenueController.java
@@ -2,11 +2,10 @@ package com.cinemaapp.backend.controller;
 
 import com.cinemaapp.backend.controller.dto.Page;
 import com.cinemaapp.backend.service.VenueService;
-import com.cinemaapp.backend.service.domain.model.Movie;
 import com.cinemaapp.backend.service.domain.model.Venue;
-import com.cinemaapp.backend.service.domain.request.CreateMovieRequest;
 import com.cinemaapp.backend.service.domain.request.CreateVenueRequest;
 import com.cinemaapp.backend.service.domain.request.SearchVenuesRequest;
+import com.cinemaapp.backend.service.domain.request.UpdateVenueRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,6 +13,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/venues")
@@ -41,5 +42,15 @@ public class VenueController {
             @Parameter(description = "Data needed for movie creation")
             @RequestBody @Valid CreateVenueRequest createVenueRequest) {
         return venueService.createVenue(createVenueRequest);
+    }
+
+    @Operation(summary = "Update venue", description = "Update a venue by its unique identifier.")
+    @PatchMapping("/{id}/edit")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Venue getMovieById(
+            @Parameter(description = "Unique identifier of the venue")
+            @PathVariable UUID id,
+            @RequestBody UpdateVenueRequest updateVenueRequest) {
+        return venueService.updateVenue(id, updateVenueRequest);
     }
 }
