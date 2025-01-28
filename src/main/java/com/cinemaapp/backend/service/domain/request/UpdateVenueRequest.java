7e519866879af52dfe0f4ef7e09cf73a0c47cadc
Avdo Sator <avdo.sator@hotmail.com>
2025-01-28 14:23:22 +0100

    Create UpdateVenueRequest class

---
+++ src/main/java/com/cinemaapp/backend/service/domain/request/UpdateVenueRequest.java
@@ -0,0 +1,61 @@
+package com.cinemaapp.backend.service.domain.request;
+
+import java.util.UUID;
+
+public class UpdateVenueRequest {
+
+    private String name;
+    private String phone;
+    private String street;
+    private String streetNumber;
+    private UUID cityId;
+    private String photoUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public UUID getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(UUID cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
