8a130bab060e4158138b8fc4d1ec1745778ebb73
Avdo Sator <avdo.sator@hotmail.com>
2025-02-02 11:14:12 +0100

    Create method for searching placeholder projection

--- src/main/java/com/cinemaapp/backend/repository/crud/CrudProjectionRepository.java
+++ src/main/java/com/cinemaapp/backend/repository/crud/CrudProjectionRepository.java
@@ -1,9 +1,11 @@
 package com.cinemaapp.backend.repository.crud;
 
+import com.cinemaapp.backend.repository.entity.MovieEntity;
 import com.cinemaapp.backend.repository.entity.ProjectionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CrudProjectionRepository extends JpaRepository<ProjectionEntity, UUID> {
+    ProjectionEntity findByMovieEntityAndStatus(MovieEntity movieEntity, String placeholder);
 }
